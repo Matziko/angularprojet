c4a13d442ab3bbae0406a27cf07fa308e7e77aca
mat <Mateoshoxha05@gmail.com>
2026-04-22 20:25:23 +0200

    everything cleaner , more animation overall

--- src/app/components/todo_input/todo_input.ts
+++ src/app/components/todo_input/todo_input.ts
@@ -1,18 +1,34 @@
-import { Component } from '@angular/core';
+import { Component,OnInit  } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { TodoService } from '../../services/todo.service';
 
 @Component({
   selector: 'app-todo-input',
+  standalone: true,
   templateUrl: './todo_input.html',
   styleUrls: ['./todo_input.scss'],
   imports: [FormsModule]
 })
-export class TodoInputComponent {
+export class TodoInputComponent implements OnInit{
   newTaskText: string = '';
   selectedPriority: 'low' | 'medium' | 'high' = 'low';
+  headerPhrase: string = '';
 
+
+private phrases: string[] = [
+    "What's the plan for today?",
+    "Got something new to achieve?",
+    "Type it, do it, crush it.",
+    "Let's get organized!",
+    "One task at a time..."
+  ];
+  
   constructor(private todoService: TodoService) {}
+
+ngOnInit() {
+    const index = Math.floor(Math.random() * this.phrases.length);
+    this.headerPhrase = this.phrases[index];
+  }
 
   setPriority(p: 'low' | 'medium' | 'high') {
     this.selectedPriority = p;
